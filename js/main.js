5f69e8b254ad74f7def69f13b40b7a29e9459dfd
Maiccol Zurita <maiccolfrancisco@gmail.com>
2024-10-17 18:19:40 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Escribiendo en el documento
+document.write("Hola mundo<br>");
+document.write("Hola mundo<br>");
+document.write("Hola mundo<br>");
+
+// Descripción del ciclo for
+/* 
+For es un ciclo que tiene 3 parámetros:
+la función es repetir un bloque de código las veces necesarias.
+for (inicialización; condición; incremento) {
+    bloque de código 
+}
+*/
+
+// Ciclo for para imprimir números pares del 3 al 15
+for (let i = 3; i <= 15; i++) {
+    // Verifica si el número es par
+
+    //document.write( "Hola desde js" + "<br>");
+    //document.write(i+"<br>");
+    if
+        (i%2==0){
+            document.write("número par: " + i + "<br>" );
+        }
+}
